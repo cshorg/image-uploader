06cb0a4e6c65361c77efcfed289064057cedbdbb
cshorg <colegrohs@outlook.com>
2023-07-31 23:23:24 -0700

    majority done, needs front-end fixes + link build

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 const Image = require("../database/schemas/Image")
 const upload = require("../routes/uploadMiddleware")
 
@@ -6,12 +7,29 @@ const router = express.Router()
 
 module.exports = router
 
-//gets image
-router.get("/:imageName", async (req, res) => {})
+router.get("/:imageName", async (req, res) => {
+  const imageName = req.params.imageName
 
-//create image link
+  try {
+    const image = await Image.findOne({ image: imageName })
+
+    if (!image) {
+      return res.status(404).json({ error: "Image not found" })
+    }
+
+    const imagePath = path.join(__dirname, "../images/", image.image)
+
+    res.sendFile(imagePath)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+})
+
 router.post("/upload", upload.single("file"), async (req, res) => {
-  Image.create({ image: req.file.filename })
-    .then((image) => res.json(image))
-    .catch((err) => console.log(err))
+  try {
+    const createdImage = await Image.create({ image: req.file.filename })
+    res.json(createdImage)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
 })
